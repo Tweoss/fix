6f1020f8bb154b9ea59364340dbc8d71525be170
tweoss <fchua@puffer5.stanford.edu>
2023-07-21 16:37:07 -0700

    add fixcache docs

--- src/storage/fixcache.hh
+++ src/storage/fixcache.hh
@@ -15,6 +15,9 @@
 class fixcache
 {
 public:
+  /// Any function which takes a Task as its argument and returns void.
+  /// Used to schedule tasks to be run. 
+  /// Required for liveness to call `cache()` when a task is finished. 
   using QueueFunction = std::function<void( Task )>;
 
 private:
@@ -24,8 +27,14 @@ private:
     dependency_cache_;
   absl::flat_hash_map<Task, std::shared_ptr<std::atomic<std::size_t>>, absl::Hash<Task>> blocked_count_;
   std::shared_mutex fixcache_mutex_;
+  /// Condition variable that is notified every time a Task finishes.
   std::condition_variable_any fixcache_cv_;
 
+  /// Mark `depender` as depending on `dependee`. 
+
+  /// Uses multidependency index if there are multiple dependers on a single 
+  /// dependee. A unique index is selected by linear scan.
+  /// @throws runtime_error if dependee equals depender.
   void insert_dependency( Task dependee, Task depender )
   {
     if ( dependee == depender ) {
@@ -42,6 +51,11 @@ private:
     }
   }
 
+  /// Requeue tasks that depend on `finished` using provided `QueueFunction`.
+
+  /// Uses multidependency index to find all depending tasks efficiently.
+  /// Reduces the count for each depending task (requestor) by 1 and if the 
+  /// count reaches 0, will queue that task.
   void unblock_jobs( Task finished, QueueFunction queue )
   {
     for ( int i = 1;; ++i ) {
@@ -58,6 +72,11 @@ private:
     }
   }
 
+  /// If `task` does not exist in cache, inserts and queues the task.
+
+  /// Initializes the fixcache entry to nullopt to signal that the task is 
+  /// queued and sets the blocking count to be 0. 
+  /// @returns True if the task is queued and inserted. Otherwise false.
   bool add_task( Task task, QueueFunction queue )
   {
     if ( not fixcache_.contains( task ) ) {
@@ -88,6 +107,11 @@ public:
     }
   }
 
+  /// Stores the result of a computation in fixcache and may unblock dependers.
+
+  /// Decreases the count and possibly requeues depending tasks. 
+  /// @throws runtime_error if the cache already contains this value.
+  /// @throws runtime_error if the task is still marked as blocked.
   void cache( Task task, Handle result, QueueFunction queue )
   {
     std::unique_lock lock( fixcache_mutex_ );
@@ -102,12 +126,20 @@ public:
     unblock_jobs( task, queue );
   }
 
+  /// Starts a task if not already in cache. 
   void start( Task task, QueueFunction queue )
   {
     std::unique_lock lock( fixcache_mutex_ );
     add_task( task, queue );
   }
 
+  /// Queues dependee if not queued and marks dependency if not finished.
+
+  /// Adds the dependee to queue if it is not already queued.
+  /// Inserts dependency of depender on dependee and increments blocked count 
+  /// of depender if dependee is not already cached. 
+  /// @returns { result } of computing dependee if already cached.
+  /// @returns {} otherwise.
   std::optional<Handle> get_or_add_dependency( Task dependee, Task depender, QueueFunction queue )
   {
     std::unique_lock lock( fixcache_mutex_ );
@@ -120,12 +152,18 @@ public:
     return {};
   }
 
+  /// Increasing blocking_count of task.
   void increment_blocking_count( Task task, std::size_t count = 1 )
   {
     std::shared_lock lock( fixcache_mutex_ );
     blocked_count_.at( task )->fetch_add( count );
   }
 
+  /// Queues dependee if not already queued. Insert dependency if not finished. 
+
+  /// Decreases the blocking count if dependee if already finished.
+  /// Otherwise, inserts the dependency.
+  /// @returns the new blocked_count value
   size_t add_dependency_or_decrement_blocking_count( Task dependee, Task depender, QueueFunction queue )
   {
     std::unique_lock lock( fixcache_mutex_ );
@@ -137,6 +175,9 @@ public:
     return *blocked_count_.at( depender );
   }
 
+  /// Blockingly waits for the target task to finish.
+
+  /// @returns the resulting Handle
   Handle get_blocking( Task target )
   {
     using namespace std;
